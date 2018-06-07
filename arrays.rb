0d779f2487dd0add4bfc3d97bba93d9f8590a822
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-07 17:53:10 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,10 @@
+
+student_names= ["maddie", "maya"] #the index of an array starts at 0 
+#puts student_names
+puts student_names[0]
+# to add something to an array use .push
+student_names.push("katelyn")
+student_names.push("maya")
+student_names.push("beyonce")
+
+puts student_names
--- reading_arrays.rb
+++ reading_arrays.rb
@@ -1,32 +1,38 @@
-STUDENT_NAMES = [
-  "Adele",
-  "Beyoncé",
-  "Cardi B",
-  "Lady Gaga",
-  "Nicki Minaj",
-  "Rihanna"
-]
+STUDENT_NAMES = [0]
+  ["Adele", beyonce]
+  ["Beyoncé"]
+  ["Cardi B"]
+  ["Lady Gaga"]
+  ["Nicki Minaj"]
+  ["Rihanna"]
+
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts #student_names "Adele"
 end
 
 def fourth_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts #student_names "Lady Gaga"
 end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts #student_names "rhianna"
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  puts #student_names "Beyoncé"
 end
 
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
+  puts #student_names"nicki Minaj"
 end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
+  puts #student_names "cardi b"
 end
